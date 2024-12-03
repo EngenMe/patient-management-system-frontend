7b009c6e0bc8c858cd7b15a5f3eee84cdfebfd5f
EngenMe <mohamed_farouk.hasnaoui@g.enp.edu.dz>
2024-12-03 12:19:30 +0000

    Refactoring the code: moving doctors fetch to another file.

--- components/NewPatientPageForm.tsx
+++ components/NewPatientPageForm.tsx
@@ -12,7 +12,7 @@ import { subYears } from 'date-fns';
 import ComboGroup from './RadioGroup';
 import ComboBox from './ComboBox';
 import { ComboBoxItem } from '@/interfaces/ComboBoxItem.interface';
-import { Doctor } from '@/interfaces/Doctor.interface';
+import { fetchDoctors } from '@/utils/fetchDoctors';
 
 const NewPatientPageForm = () => {
     const patientData = usePatientStore((state) => state.patientData);
@@ -23,32 +23,7 @@ const NewPatientPageForm = () => {
     const [doctors, setDoctors] = useState<ComboBoxItem[]>([]);
 
     useEffect(() => {
-        const fetchDoctors = async () => {
-            try {
-                const response = await fetch('http://localhost:4000/api/doctors');
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                const data = await response.json();
-
-                const doctorsArray: Doctor[] = data['data'];
-
-                const transformedDoctors = doctorsArray.map((doctor) => ({
-                    value: doctor.name
-                        .toLowerCase()
-                        .replace(/[^a-z0-9\s]/g, '')
-                        .replace(/\s+/g, '-'),
-                    label: doctor.name,
-                    picturePath: `/doctors/${doctor.picturePath}`,
-                }));
-
-                setDoctors(transformedDoctors);
-            } catch (error) {
-                console.error('Error fetching doctors:', error);
-            }
-        };
-
-        fetchDoctors();
+        fetchDoctors(setDoctors);
     }, []);
 
     const {
