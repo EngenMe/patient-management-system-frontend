6d9800eed870da8a2d1bda1b02199bfb0396b9b1
EngenMe <mohamdfarouk727@gmail.com>
2024-12-21 00:11:05 +0000

    Update theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,39 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import { ThemeProvider } from '@/components/ui/theme-provider';
-import { Plus_Jakarta_Sans } from '@next/font/google';
-import { Toaster } from '@/components/ui/toaster';
-import BreadCrumb from '@/components/BreadCrumb';
+import type { Metadata } from "next";
+import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { Plus_Jakarta_Sans } from "@next/font/google";
+import { Toaster } from "@/components/ui/toaster";
+import BreadCrumb from "@/components/BreadCrumb";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
-    weight: ['400', '500', '600', '700'],
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
-    title: 'CarePulse',
-    description: 'Patient management system to book appointments in CarePulse Ireland',
+  title: "CarePulse",
+  description:
+    "Patient management system to book appointments in CarePulse Ireland",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body className={plusJakartaSans.className}>
-                <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-                    <BreadCrumb />
-                    {children}
-                </ThemeProvider>
-                <Toaster />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={plusJakartaSans.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <BreadCrumb />
+          {children}
+        </ThemeProvider>
+        <Toaster />
+      </body>
+    </html>
+  );
 }
